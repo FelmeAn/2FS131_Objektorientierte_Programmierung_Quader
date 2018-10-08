517e801eb77688b876666565df0a375ef48363d9
FelmeAn <server.bambino@gmail.com>
2018-10-08 12:16:41 +0200

    + Quader.java   + Construct

---
+++ src/main/Quader.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Quader {
+    private double laenge,breite,hoehe;
+    public Quader(double laenge, double breite, double hoehe){
+        this.laenge=laenge;
+        this.breite=breite;
+        this.hoehe=hoehe;
+    }
+}
