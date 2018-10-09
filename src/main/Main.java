7c441975bd4298dcf93a5f59aceaf729ad2d7548
FelmeAn <server.bambino@gmail.com>
2018-10-09 07:43:11 +0200

    / Quader.java / Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,19 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-
+        double laenge,breite,hoehe;
+        Scanner sc = new Scanner(System.in);
+        laenge=input(sc, "Bitte gebe eine Länge an");
+        breite=input(sc, "Bitte gebe eine Breite an");
+        hoehe=input(sc, "Bitte gebe eine Höhe an");
+        Quader q1 = new Quader(laenge,breite,hoehe);
+        q1.flaecheBerechnenAusgeben();
+    }
+    private static double input(Scanner sc, String text){
+        System.out.print(text+": ");
+        return Double.parseDouble(sc.nextLine().replace("\\D", ""));
     }
 }
--- src/main/Quader.java
+++ src/main/Quader.java
@@ -7,4 +7,20 @@ public class Quader {
         this.breite=breite;
         this.hoehe=hoehe;
     }
+    public void flaecheBerechnenAusgeben(){
+        System.out.println("-=[DATEN]=-");
+        System.out.println("Länge: "+laenge);
+        System.out.println("Breite: "+breite);
+        System.out.println("Höhe: "+hoehe);
+        System.out.println("-=[AUSGABE]=-");
+        System.out.println("Fläche: "+(laenge*breite));
+        System.out.println("Umfang: "+umfangBerechnen());
+        System.out.println("Volumen: "+volumenBerechnen());
+    }
+    private double umfangBerechnen() {
+        return (2*laenge)+(2*breite);
+    }
+    private double volumenBerechnen() {
+        return laenge*breite*hoehe;
+    }
 }
